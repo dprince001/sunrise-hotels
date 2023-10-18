d9e9ed905d37a11119db1bbc5aedc727dfa96910
oketola samuel <oketolasamuel001@gmail.com>
2023-10-18 09:32:04 +0100

    fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
-"use client"
 import './globals.css'
-import { useEffect } from 'react';
 import Footer from './components/footer'
-import { usePathname } from 'next/navigation';
 
 
 export const metadata = {
@@ -12,14 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 
-  const pathname = usePathname();
 
-  useEffect(() => {
-    document.scrollingElement?.scroll(0, 0);
-    window.scrollTo(0,0)
-  }, [pathname]);
-
-  console.log(pathname)
 
   return (
     <html lang="en">
